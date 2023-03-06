79047aacfbcc0e73ef6c2f35447bbe0fc86bcd99
Andrew Padgett <101520702+Singular12@users.noreply.github.com>
2023-03-05 21:27:51 -0800

    Create PlusAndNegative.jsx

---
+++ PlusAndNegative.jsx
@@ -0,0 +1,32 @@
+ // Index file
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// App component
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
